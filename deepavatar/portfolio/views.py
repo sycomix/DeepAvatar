a22222a2cb1a31a0d62b129ce9988d965c1eba3d
Sourcery AI <>
2023-11-05 08:46:43 +0000

    'Refactored by Sourcery'

--- deepavatar/portfolio/views.py
+++ deepavatar/portfolio/views.py
@@ -64,110 +64,111 @@ def start_avatar_chat_to_user(request):
 		return JsonResponse(response)
 
 def check_for_new_posted_video(request):
-	if request.method == "POST":
-		feed_object_id = request.POST.get("feed_object_id", None)
-		
-		if feed_object_id != "OFF":
-			the_feed = AvatarFeed.objects.filter(pk=feed_object_id).first()
-			video_changed = the_feed.video_changed
-			first_video = the_feed.first_video
-			# print("video_changed")
-			# print(video_changed)
-			# print("first_video")
-			# print(first_video)
+	if request.method != "POST":
+		return
+	feed_object_id = request.POST.get("feed_object_id", None)
 
-			if video_changed == "True":
-				new_video = "True"
-				video_path = the_feed.video_path
-				the_feed.video_changed = "False"
-				the_feed.save(update_fields=["video_changed"])
-				video_path = generate_presigned_url(video_path)
-			else:
-				new_video = "False"
-				video_path = "None"
+	if feed_object_id != "OFF":
+		the_feed = AvatarFeed.objects.filter(pk=feed_object_id).first()
+		video_changed = the_feed.video_changed
+		first_video = the_feed.first_video
+		# print("video_changed")
+		# print(video_changed)
+		# print("first_video")
+		# print(first_video)
+
+		if video_changed == "True":
+			new_video = "True"
+			video_path = the_feed.video_path
+			the_feed.video_changed = "False"
+			the_feed.save(update_fields=["video_changed"])
+			video_path = generate_presigned_url(video_path)
 		else:
 			new_video = "False"
-			first_video = "False"
 			video_path = "None"
+	else:
+		new_video = "False"
+		first_video = "False"
+		video_path = "None"
 
-		response = {
-			'new_video': new_video,
-			'first_video': first_video,
-			'video_path': video_path,
-		}
+	response = {
+		'new_video': new_video,
+		'first_video': first_video,
+		'video_path': video_path,
+	}
 
-		return JsonResponse(response)
+	return JsonResponse(response)
 
 def get_avatar_response(request):
-	if request.method == "POST":
-		user_question_text = request.POST.get("user_question_text", None)
-		feed_object_id = request.POST.get("feed_object_id", None)
-		started_asking_questions = request.POST.get("started_asking_questions", None)
+	if request.method != "POST":
+		return
+	user_question_text = request.POST.get("user_question_text", None)
+	feed_object_id = request.POST.get("feed_object_id", None)
+	started_asking_questions = request.POST.get("started_asking_questions", None)
 
-		the_feed = AvatarFeed.objects.filter(pk=feed_object_id).first()
-		the_feed_object_id = feed_object_id
-		curr_user_question_message = the_feed.user_question_message
-		the_feed.user_question_message = user_question_text
-		the_feed.save(update_fields=["user_question_message"])
+	the_feed = AvatarFeed.objects.filter(pk=feed_object_id).first()
+	the_feed_object_id = feed_object_id
+	curr_user_question_message = the_feed.user_question_message
+	the_feed.user_question_message = user_question_text
+	the_feed.save(update_fields=["user_question_message"])
 
-		# First Question!
-		#scripts_path = os.path.join(os.getcwd(), "Scripts.xlsx")
-		scripts_path = "/mnt/avatar_storage/Scripts.xlsx"
-		embedded_scripts_path = "/mnt/avatar_storage/Scripts_embedding.csv"
-		script_finder = ScriptFinder(embedded_scripts_path)
+	# First Question!
+	#scripts_path = os.path.join(os.getcwd(), "Scripts.xlsx")
+	scripts_path = "/mnt/avatar_storage/Scripts.xlsx"
+	script_finder = ScriptFinder("/mnt/avatar_storage/Scripts_embedding.csv")
 
-		#script_finder.generate_embeddings_from_scripts()
-		
-		the_feed = AvatarFeed.objects.filter(pk=the_feed_object_id).first()
-		new_user_question_message = the_feed.user_question_message
+	#script_finder.generate_embeddings_from_scripts()
 
-		# if new_user_question_message != curr_user_question_message:
-		# 	curr_user_question_message = new_user_question_message
+	the_feed = AvatarFeed.objects.filter(pk=the_feed_object_id).first()
+	new_user_question_message = the_feed.user_question_message
 
-		# 	video_path = video_paths[new_user_question_message]
-			
-		# 	the_feed.video_path = video_path
-		# 	the_feed.video_changed = "True"
-		# 	the_feed.first_video = "False"
-		# 	the_feed.save(update_fields=["video_path", "video_changed", "first_video"])
-		# elif first_question == True:
-		# 	first_question = False
+	# if new_user_question_message != curr_user_question_message:
+	# 	curr_user_question_message = new_user_question_message
 
-		# 	video_path = video_paths[new_user_question_message]
-			
-		# 	the_feed.video_path = video_path
-		# 	the_feed.video_changed = "True"
-		# 	the_feed.first_video = "True"
-		# 	the_feed.save(update_fields=["video_path", "video_changed", "first_video"])
+	# 	video_path = video_paths[new_user_question_message]
 
-		if new_user_question_message != curr_user_question_message:
-			curr_user_question_message = new_user_question_message
-			model_response = chat(curr_user_question_message)
+	# 	the_feed.video_path = video_path
+	# 	the_feed.video_changed = "True"
+	# 	the_feed.first_video = "False"
+	# 	the_feed.save(update_fields=["video_path", "video_changed", "first_video"])
+	# elif first_question == True:
+	# 	first_question = False
 
-			# script_finder.df = pd.read_csv(embedded_scripts_path)
-			
-			query_results = script_finder.find_similar_text(model_response)
-			video_path = query_results.iloc[0]["video_path"]
-			the_feed.video_path = video_path
-			the_feed.video_changed = "True"
-			the_feed.first_video = "False"
-			the_feed.save(update_fields=["video_path", "video_changed", "first_video"])
-		# elif first_question == True:
-		# 	first_question = False
-		# 	model_response = chat(curr_user_question_message)
-			
-		# 	query_results = script_finder.find_similar_text(model_response)
-		# 	video_path = query_results.iloc[0]["video_path"]
-		# 	the_feed.video_path = video_path
-		# 	the_feed.video_changed = "True"
-		# 	the_feed.first_video = "True"
-		# 	the_feed.save(update_fields=["video_path", "video_changed", "first_video"])
-		
-		response = {
-			'msg': "success"
-		}
+	# 	video_path = video_paths[new_user_question_message]
 
-		return JsonResponse(response)
+	# 	the_feed.video_path = video_path
+	# 	the_feed.video_changed = "True"
+	# 	the_feed.first_video = "True"
+	# 	the_feed.save(update_fields=["video_path", "video_changed", "first_video"])
+
+	if new_user_question_message != curr_user_question_message:
+		curr_user_question_message = new_user_question_message
+		model_response = chat(curr_user_question_message)
+
+		# script_finder.df = pd.read_csv(embedded_scripts_path)
+
+		query_results = script_finder.find_similar_text(model_response)
+		video_path = query_results.iloc[0]["video_path"]
+		the_feed.video_path = video_path
+		the_feed.video_changed = "True"
+		the_feed.first_video = "False"
+		the_feed.save(update_fields=["video_path", "video_changed", "first_video"])
+	# elif first_question == True:
+	# 	first_question = False
+	# 	model_response = chat(curr_user_question_message)
+
+	# 	query_results = script_finder.find_similar_text(model_response)
+	# 	video_path = query_results.iloc[0]["video_path"]
+	# 	the_feed.video_path = video_path
+	# 	the_feed.video_changed = "True"
+	# 	the_feed.first_video = "True"
+	# 	the_feed.save(update_fields=["video_path", "video_changed", "first_video"])
+
+	response = {
+		'msg': "success"
+	}
+
+	return JsonResponse(response)
 
 		# if started_asking_questions == "False":
 		# 	# First Question!
